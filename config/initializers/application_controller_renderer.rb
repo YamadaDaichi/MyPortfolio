effe13944893c1981f674fa854e6f83ac65335b6
EC2 Default User <ec2-user@ip-172-31-33-184.ap-northeast-1.compute.internal>
2024-04-07 10:30:26 +0000

    update

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = '6c2fac4a0e136890aeaea8edb338ee4f5876323b1297f21becbeb461e816c58cfa01accedaf472bc17cc5d974ddaf69d32a0d3e0c679285b29c84e86e3d7a73a'
